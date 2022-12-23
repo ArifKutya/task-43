9c589fdd57ad9da77fdc793778d91584f6a14661
Arif <arifkutq63@gmail.com>
2022-12-23 11:45:38 +0200

    bev

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,3 @@
-import "../scss/app.scss";
 
-window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
 
-  setTimeout(function(){
-    document.title = "One new message"
-  }, 3000);
-  }
 
